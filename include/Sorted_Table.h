69af93df021a6fff68944c5f206dbfcba20606dd
Alexey_chekotov <alexche2509.@gmail.com>
2025-03-09 21:56:41 +0300

    Realesed Sorted table

--- include/Sorted_Table.h
+++ include/Sorted_Table.h
@@ -2,6 +2,8 @@
 #include<vector>
 #include"Polynomial.h"
 
+//TODO: write throw 
+
 template <class Tkey, class Tvalue>
 class SortedTable {
 	std::vector<std::pair<Tkey, Tvalue>> table;
@@ -10,11 +12,9 @@ public:
 
 	class iterator {
 	public:
-		std::pair<Tkey, Tvalue> ptr;
+		std::pair<Tkey, Tvalue>* ptr;
 
-		iterator(std::pair<Tkey, Tvalue>* p) {
-			ptr = p;
-		}
+		explicit iterator(std::pair<Tkey, Tvalue>* p) : ptr(p) { }
 
 		iterator& operator++() {
 			return ptr++;
@@ -22,10 +22,10 @@ public:
 		iterator& operator--() {
 			return ptr--;
 		}
-		bool operator==(iterator& it) {
+		bool operator==(const iterator& it) const{
 			return ptr == it.ptr;
 		}
-		bool operator!=(iterator& it) {
+		bool operator!=(const iterator& it) const {
 			return ptr != it.ptr;
 		}
 		std::pair<Tkey, Tvalue>& operator*() {
@@ -40,7 +40,59 @@ public:
 		return iterator(&table[0]);
 	}
 	iterator end() {
-		return iterator(&table[table.size()]);
+		return iterator(nullptr);
+	}
+	
+	void insert(Tkey key, Tvalue val) {
+		table.push_back(std::make_pair(key, val));
+		int i = table.size() - 1;
+		if (i == 0)
+			return;
+		while (i > 0) {
+			if (table[i - 1].first > table[i].first) {
+				std::swap(table[i - 1], table[i]);
+				i--;
+			}
+			else if (table[i - 1].first == table[i].first)
+				throw " ";
+			else
+				break;
+		}
 	}
 
+	iterator find(const Tkey& key) {
+		int l = 0, r = table.size() - 1;
+		while (l <= r) {
+			int center = (l + r) / 2;
+			if (table[center].first == key)
+				return iterator(&table[center]);
+
+			else if (table[center].first < key)
+				l = center + 1;
+			else
+				r = center - 1;
+		}
+		return end();
+	}
+
+	iterator erase(const Tkey& key) {
+		auto it = find(key);
+		if (it == end())
+			throw " ";
+		for (int i = 0; i < table.size();i++) {
+			if (table[i].first == key) {
+				table.erase(table.begin() + i);
+				break;
+			}
+		}
+		return it;
+	}
+
+	std::pair<Tkey, Tvalue> operator[](int ind){
+		return table[ind];
+	}
+
+	int size() {
+		return table.size();
+	}
 };
--- test/test_Sorted_Table.cpp
+++ test/test_Sorted_Table.cpp
@@ -1,2 +1,86 @@
 #include <gtest.h>
+#include <random>
 #include "Sorted_Table.h"
+
+TEST(SortedTable, CanInsert) {
+	SortedTable<std::string, polynom> t;
+	polynom p = polynom(monom(1, 100));
+	ASSERT_NO_THROW(t.insert("abc", p));
+}
+
+TEST(SortedTable, CorrectInsertionOrder) {
+	SortedTable<std::string, polynom> t;
+	polynom p = polynom(monom(1, 100));
+	t.insert("abc", p);
+	t.insert("abd", p);
+	t.insert("abb", p);
+	EXPECT_EQ("abb", t[0].first);
+	EXPECT_EQ("abc", t[1].first);
+	EXPECT_EQ("abd", t[2].first);
+}
+
+TEST(SortedTable, ThrowWhenInsertTwoIdencialElement) {
+	SortedTable<std::string, polynom> t;
+	polynom p = polynom(monom(1, 100));
+	t.insert("abc", p);
+	ASSERT_ANY_THROW(t.insert("abc", p));
+}
+
+TEST(SortedTable, CorrectFindElement) {
+	SortedTable<std::string, polynom> t;
+	polynom p = polynom(monom(1, 100));
+	t.insert("abc", p);
+	t.insert("abd", p);
+	t.insert("abb", p);
+	EXPECT_EQ(t.begin(), t.find("abb"));
+}
+
+TEST(SortedTable, NotFindNotExistingElement) {
+	SortedTable<std::string, polynom> t;
+	polynom p = polynom(monom(1, 100));
+	t.insert("abc", p);
+	t.insert("abd", p);
+	t.insert("abb", p);
+	EXPECT_EQ(t.end(), t.find("a"));
+}
+
+TEST(SortedTable, CorrectEraseElement) {
+	SortedTable<std::string, polynom> t;
+	polynom p = polynom(monom(1, 100));
+	t.insert("abc", p);
+	t.insert("abd", p);
+	t.insert("abb", p);
+	t.erase("abc");
+	EXPECT_EQ("abb", t[0].first);
+	EXPECT_EQ("abd", t[1].first);
+}
+
+TEST(SortedTable, ThrowWhenEraseNotExistingElement) {
+	SortedTable<std::string, polynom> t;
+	polynom p = polynom(monom(1, 100));
+	t.insert("abc", p);
+	t.insert("abd", p);
+	t.insert("abb", p);
+	ASSERT_ANY_THROW(t.erase("ab"));
+}
+
+TEST(SortedTable, StressTest) {
+	SortedTable<int, polynom> t;
+	polynom p = polynom(monom(1, 100));
+
+	for (int i = 0; i < 1000; i++) {
+		int r = rand()%10;
+		if (r < 7 || i < 50)
+			t.insert(i, p);
+		else {
+			int j = i / 2;
+			while (t.find(j) == t.end())
+				j--;
+			if(t.find(j) != t.end())
+				t.erase(j);
+		}
+	}
+
+	for(int i = 1;i < t.size();i++)
+		EXPECT_LE(t[i - 1].first, t[i].first);
+}
