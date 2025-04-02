0c1e348ec1b0d84ccceac031b4d458cc42711e17
Uzema <uzema_2k16@mail.ru>
2025-04-02 23:19:20 +0300

    wip

--- include/AVLTree.h
+++ include/AVLTree.h
@@ -1,1 +1,24 @@
-﻿
+﻿#pragma once
+#include <vector>
+#include "Polynomial.h"
+
+template <class Tkey, class Tvalue>
+class SearchTree {
+
+	Node* root;
+
+	struct Node {
+		std::pair<Tkey, Tvalue> data;
+		Node* left;
+		Node* right;
+		Node* parent;
+	};
+
+public:
+	Node* find(Tkey key) {
+		return find(key, root);
+	}
+
+private:
+	Node* find(Tkey key Node* cur);
+};
